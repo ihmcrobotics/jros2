09bd4cfda7b45ff3049f7bbca47de5bde757075a
Dexton Anderson <danderson@ihmc.org>
2025-04-08 14:11:12 -0500

    impl

--- src/test/java/us/ihmc/jros2/ROS2PublishSubscribeTest.java
+++ src/test/java/us/ihmc/jros2/ROS2PublishSubscribeTest.java
@@ -18,47 +18,6 @@ public class ROS2PublishSubscribeTest
    }
 
    @Test
-   public void publishSubscribeTest() throws InterruptedException
-   {
-      ROS2Node ros2Node = new ROS2Node("test_node", 113);
-      ROS2Topic<Bool> topic = new ROS2Topic<>(Bool.class, "rt/ihmc/test_bool");
-      ROS2Publisher<Bool> publisher = ros2Node.createPublisher(topic, ROS2QoSProfile.DEFAULT);
-
-      Bool bool = new Bool();
-      bool.setData(true);
-
-      int iter = 0;
-      while (iter < 10000)
-      {
-         publisher.publish(bool);
-
-         System.out.println("Wrote");
-
-         iter++;
-
-         Thread.sleep(1000);
-      }
-
-      ros2Node.close();
-   }
-
-   @Test
-   public void subscribeTest() throws InterruptedException
-   {
-      ROS2Node ros2Node = new ROS2Node("test_node", 113);
-      ROS2Topic<Bool> topic = new ROS2Topic<>(Bool.class, "rt/ihmc/test_bool");
-      ROS2Subscription<Bool> subscription = ros2Node.createSubscription(topic, (ROS2SubscriptionCallback<Bool>) reader ->
-      {
-         Bool msg = new Bool();
-         reader.takeNextSample(msg);
-
-         System.out.println(msg.getData());
-      }, ROS2QoSProfile.DEFAULT);
-
-      Thread.sleep(10000000);
-   }
-
-   @Test
    public void testROS2Publisher() throws InterruptedException, IOException
    {
       final boolean expectedValue = true;
