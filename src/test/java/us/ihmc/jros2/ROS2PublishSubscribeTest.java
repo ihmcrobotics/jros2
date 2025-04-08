199b23f7b755c170e612ef7a7e9359cf93344867
Tomasz Bialek <tbialek@ihmc.org>
2025-04-08 16:45:37 -0500

    impl

--- src/test/java/us/ihmc/jros2/ROS2PublishSubscribeTest.java
+++ src/test/java/us/ihmc/jros2/ROS2PublishSubscribeTest.java
@@ -12,12 +12,119 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.LockSupport;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ROS2PublishSubscribeTest
 {
    private static final Random RANDOM = new Random();
+
+   @Test
+   public void testROS2PublisherAPI()
+   {
+      int domainId = 113;
+      String topicName = "/ihmc/test_bool";
+
+      // Create ROS 2 node and topic
+      ROS2Node ros2Node = new ROS2Node("test_node", domainId);
+      ROS2Topic<Bool> topic = new ROS2Topic<>(Bool.class, "rt" + topicName);
+
+      assertDoesNotThrow(() ->
+      {
+         // Create publishers
+         ROS2Publisher<Bool> publisher1 = ros2Node.createPublisher(topic, ROS2QoSProfile.DEFAULT);
+         ROS2Publisher<Bool> publisher2 = ros2Node.createPublisher(topic, ROS2QoSProfile.DEFAULT);
+
+         // Try publishing concurrently
+         Thread[] publishThreads = new Thread[10];
+         for (int i = 0; i < publishThreads.length; ++i)
+         {
+            Thread thread = new Thread(() ->
+            {
+               LockSupport.parkNanos(RANDOM.nextLong((long) 1E8));
+               publisher1.publish(new Bool());
+            }, "PublishThread" + i);
+            thread.start();
+            publishThreads[i] = thread;
+         }
+
+         // Ensure all threads die
+         for (Thread thread : publishThreads)
+            thread.join(1000);
+
+         // Ensure we can destroy a publisher
+         ros2Node.destroyPublisher(publisher1);
+
+         // Oops, I "accidentally" destroyed it again
+         ros2Node.destroyPublisher(publisher1);
+
+         // Try destroying while publishing
+         Thread publishThread = new Thread(() ->
+         {
+            while (!Thread.interrupted())
+            {
+               publisher2.publish(new Bool());
+            }
+         }, ":PublishThread");
+         publishThread.start();
+
+         // Destroy the publisher while it's publishing
+         ros2Node.destroyPublisher(publisher2);
+
+         // Tell the thread to stop, and make sure it dies
+         publishThread.interrupt();
+         publishThread.join(1000);
+      });
+
+      ros2Node.close();
+   }
+
+   @Test
+   public void testROS2SubscriptionAPI()
+   {
+      int domainId = 113;
+      String topicName = "/ihmc/test_bool";
+
+      // Create ROS 2 node and topic
+      ROS2Node ros2Node = new ROS2Node("test_node", domainId);
+      ROS2Topic<Bool> topic = new ROS2Topic<>(Bool.class, "rt" + topicName);
+
+      assertDoesNotThrow(() ->
+      {
+         // Create subscriptions
+         ROS2Subscription<Bool> subscription1 = ros2Node.createSubscription(topic, (ROS2SubscriptionCallback<Bool>) reader ->
+         {
+            Bool message = new Bool();
+            reader.takeNextSample(message);
+            assert false; // Should never reach here since we don't publish anything
+         }, ROS2QoSProfile.DEFAULT);
+
+         ROS2Subscription<Bool> subscription2 = ros2Node.createSubscription(topic, (Consumer<Bool>) message ->
+         {
+            assert false; // Should never reach here since we don't publish anything
+         }, ROS2QoSProfile.DEFAULT);
+
+         // Ensure we can destroy subscriptions
+         ros2Node.destroySubscription(subscription1);
+         ros2Node.destroySubscription(subscription2);
+
+         // Oops, I "accidentally" destroyed it again
+         ros2Node.destroySubscription(subscription1);
+
+         // Publish a message to ensure subscriptions don't receive anything after being destroyed
+         Process process = ROS2TestTools.launchROS2PublishProcess(domainId,
+                                                                  "--once",
+                                                                  topicName,
+                                                                  "std_msgs/msg/Bool",
+                                                                  "{data: true}",
+                                                                  Redirect.INHERIT,
+                                                                  Redirect.INHERIT);
+         process.waitFor();
+      });
+
+      ros2Node.close();
+   }
 
    @Test
    public void testROS2Publisher() throws InterruptedException, IOException
