e44e95a3664157611f1e2f766c608a5aacdb5bce
Tomasz Bialek <tbialek@ihmc.org>
2025-05-08 16:02:25 -0500

    Added comparison test

--- src/test/java/us/ihmc/jros2/AsyncROS2Test.java
+++ src/test/java/us/ihmc/jros2/AsyncROS2Test.java
@@ -3,10 +3,13 @@ package us.ihmc.jros2;
 import org.junit.jupiter.api.Test;
 import us.ihmc.jros2.ROS2QoSProfile.Durability;
 import us.ihmc.jros2.msg.Bool;
+import us.ihmc.log.LogTools;
 
 import java.io.IOException;
+import java.util.DoubleSummaryStatistics;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -91,5 +94,126 @@ public class AsyncROS2Test
       asyncNode.destroySubscription(subscription);
       asyncNode.destroyPublisher(publisher);
       asyncNode.close();
+   }
+
+   @Test
+   public void compareStandardAndAsyncPublisher() throws InterruptedException
+   {
+      final boolean generateCSV = false;
+      final int messagesToPublish = 10000;
+      final boolean expected = true;
+
+      // Create normal and async nodes
+      ROS2Node ros2Node = new ROS2Node("standard_node");
+      AsyncROS2Node asyncROS2Node = new AsyncROS2Node("async_node");
+
+      // Create topics to publish on
+      ROS2Topic<Bool> standardTopic = new ROS2Topic<>(Bool.class, "rt/standard_topic");
+      ROS2Topic<Bool> asyncTopic = new ROS2Topic<>(Bool.class, "rt/async_topic");
+
+      // Create normal and async publishers
+      ROS2Publisher<Bool> standardPublisher = ros2Node.createPublisher(standardTopic);
+      DoubleStatisticsHelper standardPublisherStatistics = new DoubleStatisticsHelper();
+
+      ROS2Publisher<Bool> asyncPublisher = asyncROS2Node.createPublisher(asyncTopic);
+      DoubleStatisticsHelper asyncPublisherStatistics = new DoubleStatisticsHelper();
+
+      // Create subscribers
+      ROS2SubscriptionCallback<Bool> callback = reader -> assertEquals(expected, reader.read().getData());
+      ROS2Subscription<Bool> standardSubscription = ros2Node.createSubscription(standardTopic, callback);
+      ROS2Subscription<Bool> asyncSubscription = asyncROS2Node.createSubscription(asyncTopic, callback);
+
+      Bool message = new Bool();
+      message.setData(expected);
+
+      BiFunction<ROS2Publisher<Bool>, DoubleStatisticsHelper, Runnable> runnableProvider = (ros2Publisher, statistics) -> () ->
+      {
+         for (int i = 0; i < messagesToPublish; ++i)
+         {
+            long start = System.nanoTime();
+            ros2Publisher.publish(message);
+            long duration = System.nanoTime() - start;
+            statistics.accept(duration * 1E-9);
+            try
+            {
+               Thread.sleep(5);
+            }
+            catch (InterruptedException e)
+            {
+               throw new RuntimeException(e);
+            }
+         }
+      };
+
+      // Create two threads
+      Thread standardPublisherThread = new Thread(runnableProvider.apply(standardPublisher, standardPublisherStatistics));
+      Thread asyncPublisherThread = new Thread(runnableProvider.apply(asyncPublisher, asyncPublisherStatistics));
+
+      // Start both threads
+      standardPublisherThread.start();
+      asyncPublisherThread.start();
+
+      // Wait until they finish
+      standardPublisherThread.join();
+      asyncPublisherThread.join();
+
+      // Log statistics
+      LogTools.info("Standard publisher statistics: {}", standardPublisherStatistics);
+      LogTools.info("Async publisher statistics:    {}", asyncPublisherStatistics);
+
+      // Ensure both threads published all messages
+      assertEquals(messagesToPublish, asyncPublisherStatistics.getCount());
+      assertEquals(messagesToPublish, standardPublisherStatistics.getCount());
+
+      // Ensure async publisher is faster and more consistent
+      assertTrue(asyncPublisherStatistics.getAverage() < standardPublisherStatistics.getAverage());
+      assertTrue(asyncPublisherStatistics.getStandardDeviation() < standardPublisherStatistics.getStandardDeviation());
+
+      // Cleanup
+      ros2Node.destroyPublisher(standardPublisher);
+      ros2Node.destroySubscription(standardSubscription);
+      ros2Node.close();
+
+      asyncROS2Node.destroyPublisher(asyncPublisher);
+      asyncROS2Node.destroySubscription(asyncSubscription);
+      asyncROS2Node.close();
+   }
+
+   private static class DoubleStatisticsHelper extends DoubleSummaryStatistics
+   {
+      private double squaredSum;
+      private double standardDeviation;
+
+      public DoubleStatisticsHelper()
+      {
+         super();
+         squaredSum = 0.0;
+         standardDeviation = 0.0;
+      }
+
+      @Override
+      public void accept(double value)
+      {
+         super.accept(value);
+
+         squaredSum += value * value;
+         standardDeviation = Math.sqrt(getCount() * squaredSum - getSum() * getSum()) / getCount();
+      }
+
+      public double getStandardDeviation()
+      {
+         return standardDeviation;
+      }
+
+      @Override
+      public String toString()
+      {
+         StringBuilder result = new StringBuilder(super.toString());
+
+         result.replace(result.length() - 1, result.length(), ", ");
+         result.append(standardDeviation).append("}");
+
+         return result.toString();
+      }
    }
 }
