de7968c380c9d78045309f308a847a4c6ba3d0c9
Dexton Anderson <danderson@ihmc.org>
2025-05-21 11:52:30 -0500

    Fix typo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,4 @@ include("examples:ros2-opencv-webcam")
 findProject(":examples:ros2-opencv-webcam")?.name = "ros2-opencv-webcam"
 
 include("examples:ros2-talker-listener")
-findProject(":examples:ros2-talker-listener")?.name = "ros2-ros2-talker-listener"
+findProject(":examples:ros2-talker-listener")?.name = "ros2-talker-listener"
