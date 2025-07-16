b748981a2c497c2bcb5f90472efc2b28bb39ee9d
Dexton Anderson <danderson@ihmc.org>
2025-07-16 15:08:53 -0500

    Test

--- src/test/java/us/ihmc/jros2/jros2SettingsTest.java
+++ src/test/java/us/ihmc/jros2/jros2SettingsTest.java
@@ -1,5 +1,6 @@
 package us.ihmc.jros2;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import us.ihmc.log.LogTools;
 
@@ -20,6 +21,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+@Disabled
 public class jros2SettingsTest
 {
    @Test
