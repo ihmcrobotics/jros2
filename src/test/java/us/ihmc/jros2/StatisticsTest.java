beba6c43291d8a836057ec619f0b81781505c987
Dexton Anderson <danderson@ihmc.org>
2025-07-16 17:10:57 -0500

    Test

--- src/test/java/us/ihmc/jros2/StatisticsTest.java
+++ src/test/java/us/ihmc/jros2/StatisticsTest.java
@@ -164,8 +164,11 @@ public class StatisticsTest
       // Read the message publish period statistics from the publisher, and assert they make sense
       double expectedPeriod = 100.0;
       publisher.readStatistics(MessageMetadataType.PERIOD, statistics);
+      System.out.println("EXPECTED PERIOD" + expectedPeriod);
       assertEquals(expectedPeriod, statistics.get(AVERAGE), expectedPeriod / 2.0);
+      System.out.println("AVG PERIOD " + statistics.get(AVERAGE));
       assertEquals(expectedPeriod, statistics.get(MINIMUM), expectedPeriod / 2.0);
+      System.out.println("MIN PERIOD " + statistics.get(MINIMUM));
       assertEquals(expectedPeriod, statistics.get(MAXIMUM), expectedPeriod / 2.0);
       assertEquals(publishCount.get() - 1, statistics.get(SAMPLE_COUNT), 1E-7);
       assertEquals((publishCount.get() - 1) * expectedPeriod, statistics.get(TOTAL), 20.0);
