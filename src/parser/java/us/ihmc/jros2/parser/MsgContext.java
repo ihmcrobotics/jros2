6f87e6f7b18167e7c82bba0c4665b9775617594d
Dexton Anderson <danderson@ihmc.org>
2025-12-18 15:00:26 -0600

    Revert "Add enum class generation (#75)"
    This reverts commit ff2ab0f2cf8abe757172d32ac4520f0ff4e1e1bd.

--- src/parser/java/us/ihmc/jros2/parser/MsgContext.java
+++ src/parser/java/us/ihmc/jros2/parser/MsgContext.java
@@ -15,77 +15,10 @@
  */
 package us.ihmc.jros2.parser;
 
-import us.ihmc.jros2.parser.field.InterfaceField;
-
-import java.util.LinkedList;
-import java.util.List;
-
 public class MsgContext extends InterfaceContext
 {
    protected MsgContext(String schema)
    {
       super(schema);
-   }
-
-   public boolean isEnum()
-   {
-      int uint8_constantFieldsCount = 0;
-      int uint8_otherFieldsCount = 0;
-      int otherFieldsCount = 0;
-
-      for (InterfaceField field : getFieldList())
-      {
-         if (field.getType().equals("uint8"))
-         {
-            if (field.getConstantValue() != null)
-            {
-               uint8_constantFieldsCount++;
-            }
-            else
-            {
-               uint8_otherFieldsCount++;
-            }
-         }
-         else
-         {
-            otherFieldsCount++;
-         }
-      }
-
-      /*
-       * We assume a message interface is an enum if there are more than 0 uint8 constant fields,
-       * exactly 1 uint8 fields, and no other fields.
-       */
-      return uint8_constantFieldsCount > 0 && uint8_otherFieldsCount == 1 && otherFieldsCount == 0;
-   }
-
-   public List<InterfaceField> getEnumFields()
-   {
-      List<InterfaceField> enumFields = new LinkedList<>();
-
-      if (isEnum())
-      {
-         for (InterfaceField field : getFieldList())
-         {
-            if (field.getConstantValue() != null)
-            {
-               enumFields.add(field);
-            }
-         }
-      }
-
-      return enumFields;
-   }
-
-   public List<String> getEnumNames()
-   {
-      List<String> enumNames = new LinkedList<>();
-
-      for (InterfaceField enumField : getEnumFields())
-      {
-         enumNames.add(enumField.getName());
-      }
-
-      return enumNames;
    }
 }
