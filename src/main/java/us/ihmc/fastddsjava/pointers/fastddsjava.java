5855ba900ec1af0d55b6ecb04bb0d8fbb1942d3c
ihmc-rosie <rosie@ihmc.us>
2026-03-25 15:38:57 -0500

    Rebuild natives for Linux

--- src/main/java/us/ihmc/fastddsjava/pointers/fastddsjava.java
+++ src/main/java/us/ihmc/fastddsjava/pointers/fastddsjava.java
@@ -126,7 +126,7 @@ public static native @Cast("uint32_t") int fastddsjava_datawriter_write(Pointer 
 public static native Pointer fastddsjava_create_datareader(Pointer subscriber_, Pointer topic_, fastddsjava_DataReaderListener listener, @StdString BytePointer profile_name);
 public static native Pointer fastddsjava_create_datareader(Pointer subscriber_, Pointer topic_, fastddsjava_DataReaderListener listener, @StdString String profile_name);
 
-public static native @Cast("uint32_t") int fastddsjava_datareader_read_next_custom(Pointer reader_, Pointer data, Pointer info_);
+public static native @Cast("uint32_t") int fastddsjava_datareader_read_next_sample(Pointer reader_, Pointer data, Pointer info_);
 
 public static native @Cast("uint32_t") int fastddsjava_datareader_take_next_custom(Pointer reader_, Pointer data, Pointer info_);
 
